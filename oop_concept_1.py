4e0f4eaccae1f795917d1be3fa83b3a6a6c1d7f3
10nac <tenacious934495@gmail.com>
2025-07-10 09:06:24 +0530

    added oop concept file

---
+++ oop_concept_1.py
@@ -0,0 +1,24 @@
+##class Robot:
+##    x=Robot()
+##    Y=Robot()
+##    x.name = "sathish"
+##    y.name = "vignesh"
+##    x.age = 56
+##    y.age = 78
+##    print(x.age)
+##    print(y.name)
+##    print(x.__dict__)
+
+
+class friends:
+    name = friends()
+    place = friends()
+    name.1 = "sathish"
+    name.2 = "dhinesh"
+    name.3 = "madhavan"
+    place.1 ="visalur"
+    place.2 = "visalur"
+    name.3 = "visalur"
+    print(name.__dict__)
+    print(place.__dict__)
+    
